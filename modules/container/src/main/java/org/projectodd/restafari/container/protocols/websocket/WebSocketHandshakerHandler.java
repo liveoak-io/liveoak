688d248ea7c409f7d09828545b130927c5c734fa
nscavell <nick.scavelli@gmail.com>
2013-10-04 17:05:43 -0400

    Use 'Upgrade' header instead of requiring /_websockets to be part of the path.

--- modules/container/src/main/java/org/projectodd/restafari/container/protocols/websocket/WebSocketHandshakerHandler.java
+++ modules/container/src/main/java/org/projectodd/restafari/container/protocols/websocket/WebSocketHandshakerHandler.java
@@ -4,6 +4,7 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.HttpHeaders;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.websocketx.WebSocketServerHandshaker;
 import io.netty.handler.codec.http.websocketx.WebSocketServerHandshakerFactory;
@@ -15,7 +16,7 @@ import org.projectodd.restafari.container.protocols.PipelineConfigurator;
  */
 public class WebSocketHandshakerHandler extends SimpleChannelInboundHandler<Object> {
 
-    private static final String WEBSOCKETS_PREFIX = "/_websockets";
+    private static final String WEBSOCKETS_UPGRADE = "websocket";
 
     public WebSocketHandshakerHandler(PipelineConfigurator configurator) {
         this.configurator = configurator;
@@ -25,9 +26,9 @@ public class WebSocketHandshakerHandler extends SimpleChannelInboundHandler<Obje
     protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
 
         FullHttpRequest req = (FullHttpRequest) msg;
-
-        if (req.getUri().startsWith(WEBSOCKETS_PREFIX)) {
-            WebSocketServerHandshakerFactory wsFactory = new WebSocketServerHandshakerFactory(WEBSOCKETS_PREFIX, null, false);
+        String upgrade = req.headers().get(HttpHeaders.Names.UPGRADE);
+        if (WEBSOCKETS_UPGRADE.equals(upgrade)) {
+            WebSocketServerHandshakerFactory wsFactory = new WebSocketServerHandshakerFactory(req.getUri(), null, false);
             WebSocketServerHandshaker handshaker = wsFactory.newHandshaker(req);
             if (handshaker == null) {
                 WebSocketServerHandshakerFactory.sendUnsupportedWebSocketVersionResponse(ctx.channel());
