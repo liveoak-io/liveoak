0061e0dcd2896833bcaca73e058c05bf34209c66
Marko Strukelj <marko.strukelj@gmail.com>
2013-10-11 00:54:21 +0200

    Starter module - use it to run a standalone server

---
+++ modules/starter/src/main/java/org/projectodd/restafari/mongo/ServerStarterVerticle.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2013 Red Hat, Inc.
+ *
+ * Red Hat licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * @author <a href="marko.strukelj@gmail.com">Marko Strukelj</a>
+ */
+package org.projectodd.restafari.mongo;
+
+import io.netty.channel.nio.NioEventLoopGroup;
+import org.projectodd.restafari.container.Container;
+import org.projectodd.restafari.container.SimpleConfig;
+import org.projectodd.restafari.container.UnsecureServer;
+import org.projectodd.restafari.deployer.DeployerController;
+import org.projectodd.restafari.spi.InitializationException;
+import org.vertx.java.core.json.JsonObject;
+import org.vertx.java.platform.Verticle;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/*
+This is a simple Java verticle which starts the server
+ */
+public class ServerStarterVerticle extends Verticle {
+
+    private UnsecureServer server;
+
+    @Override
+    public void start() {
+
+        Container container = new Container();
+        try {
+            // mongo db configuration
+            JsonObject conf = getConf("storage");
+            SimpleConfig config = new SimpleConfig();
+            setNonNullOnly(config, "db", getWithFailOver(System.getProperty("mbaas.mongo.db"), conf.getString("db"), "mydb"));
+            setNonNullOnly(config, "host", getWithFailOver(System.getProperty("mbaas.mongo.host"), conf.getString("host"), "localhost"));
+            setNonNullOnly(config, "port", getWithFailOver(System.getProperty("mbaas.mongo.port"), conf.getString("port")));
+
+            container.registerResourceController("storage", new MongoController(), config);
+        } catch (InitializationException e) {
+            throw new RuntimeException("Failed to initialize resource controller: MongoController", e);
+        }
+
+        try {
+            // deployer configuration
+            JsonObject conf = getConf("deployer");
+            SimpleConfig config = new SimpleConfig();
+            setNonNullOnly(config, "mbaas.deployer.dir", getWithFailOver(
+                    System.getProperty("mbaas.deployer.dir"),
+                    conf.getString("dir"),
+                    System.getProperty("user.dir")));
+
+            container.registerResourceController("deployer", new DeployerController(), config);
+        } catch (InitializationException e) {
+            throw new RuntimeException("Failed to initialize resource controller: DeployerController", e);
+        }
+
+        try {
+            server = new UnsecureServer(container, InetAddress.getByName("localhost"), 8080, new NioEventLoopGroup());
+        } catch (UnknownHostException e) {
+            throw new RuntimeException("Failed to bind server to localhost: ", e);
+        }
+
+        try {
+            server.start();
+        } catch (InterruptedException e) {
+            throw new RuntimeException("Must exit - interrupted");
+        }
+
+        System.err.println("ServerStarterVerticle started");
+    }
+
+    @Override
+    public void stop() {
+        if (server == null)
+            return;
+
+        try {
+            server.stop();
+        } catch (InterruptedException ignored) {
+        }
+    }
+
+    private JsonObject getConf(String name) {
+
+        org.vertx.java.platform.Container cont = getContainer();
+        JsonObject conf = cont != null ? cont.config() : null;
+
+        if (conf != null) {
+            conf = conf.getObject(name);
+        }
+        return conf != null ? conf : new JsonObject();
+    }
+
+    private <T> T getWithFailOver(T... val) {
+        for (T v: val) {
+            if (v != null && String.valueOf(v).length() > 0)
+                return v;
+        }
+        return null;
+    }
+
+    private void setNonNullOnly(SimpleConfig conf, String key, String value) {
+        if (value != null)
+            conf.put(key, value);
+    }
+}
---
+++ modules/starter/src/test/java/org/projectodd/restafari/mongo/integration/java/ModuleIntegrationTest.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2013 Red Hat, Inc.
+ *
+ * Red Hat licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package org.projectodd.restafari.mongo.integration.java;
+
+import org.junit.Test;
+import org.vertx.java.core.AsyncResult;
+import org.vertx.java.core.AsyncResultHandler;
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.http.HttpClientResponse;
+import org.vertx.testtools.TestVerticle;
+
+import static org.vertx.testtools.VertxAssert.*;
+import static org.vertx.testtools.VertxAssert.assertEquals;
+
+/**
+ * Example Java integration test that deploys the module that this project builds.
+ * <p/>
+ * Quite often in integration tests you want to deploy the same module for all tests and you don't want tests
+ * to start before the module has been deployed.
+ * <p/>
+ * This test demonstrates how to do that.
+ */
+public class ModuleIntegrationTest extends TestVerticle {
+
+    @Test
+    public void testServerUp() {
+        container.logger().info("- testServerUp()");
+
+        vertx.createHttpClient()
+            .setHost("localhost")
+            .setPort(8080)
+            .getNow("/storage/", new Handler<HttpClientResponse>() {
+                public void handle(HttpClientResponse response) {
+                    assertEquals(404, response.statusCode());
+                    testComplete();
+                }
+        });
+    }
+
+    @Override
+    public void start() {
+        // Make sure we call initialize() - this sets up the
+        // assert stuff so assert functionality works correctly
+        initialize();
+
+        // Deploy the module - the System property `vertx.modulename`
+        // will contain the name of the module so you
+        // don't have to hardecode it in your tests
+        container.deployModule(System.getProperty("vertx.modulename"), new AsyncResultHandler<String>() {
+            @Override
+            public void handle(AsyncResult<String> asyncResult) {
+                // Deployment is asynchronous and this this handler will be called when it's complete (or failed)
+                assertTrue(asyncResult.succeeded());
+                assertNotNull("deploymentID should not be null", asyncResult.result());
+                // If deployed correctly then start the tests!
+                startTests();
+            }
+        });
+    }
+
+}
