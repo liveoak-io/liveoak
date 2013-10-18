b3d958c1cca9b3cfa150b5bc8de4b96133bd728a
Bob McWhirter <bob@mcwhirter.org>
2013-10-18 12:29:07 -0400

    Wire up some async assertion helpering.

---
+++ modules/container/src/test/java/org/projectodd/restafari/container/AssertionHelper.java
@@ -0,0 +1,41 @@
+package org.projectodd.restafari.container;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Bob McWhirter
+ */
+public class AssertionHelper {
+
+    public static interface AssertionBlock {
+        public void doAssertions() throws Throwable;
+    }
+
+    public void assertThat(AssertionBlock b) {
+        try {
+            b.doAssertions();
+        } catch (Throwable e) {
+            System.err.println( "caught: " + e );
+            errors.add(e);
+        }
+    }
+
+    public void complete() throws Throwable {
+        if (errors.isEmpty()) {
+            return;
+        }
+
+        if (errors.get(0) instanceof AssertionError) {
+            throw errors.get(0);
+        }
+
+        throw new AssertionError(errors.get(0));
+
+    }
+
+    private List<Throwable> errors = new ArrayList<>();
+}
--- modules/container/src/test/java/org/projectodd/restafari/container/DirectConnectorTest.java
+++ modules/container/src/test/java/org/projectodd/restafari/container/DirectConnectorTest.java
@@ -1,15 +1,23 @@
 package org.projectodd.restafari.container;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.concurrent.CountDownLatch;
+
+import static org.fest.assertions.Assertions.*;
 
 /**
  * @author Bob McWhirter
  */
 public class DirectConnectorTest {
 
+
+    private AssertionHelper assertionHelper = new AssertionHelper();
     private DefaultContainer container;
     private DirectConnector connector;
+
 
     @Before
     public void setUp() throws Exception {
@@ -17,16 +25,36 @@ public class DirectConnectorTest {
         this.connector = this.container.directConnector();
     }
 
+    @After
+    public void checkAssertions() throws Throwable {
+        this.assertionHelper.complete();
+    }
+
+    protected void assertions(AssertionHelper.AssertionBlock b) {
+        this.assertionHelper.assertThat(b);
+    }
+
     @Test
-    public void testConnectorRead() {
+    public void testConnectorRead() throws Throwable {
+
+        CountDownLatch latch = new CountDownLatch(2);
 
         this.connector.read("/", (response) -> {
-            System.err.println( "response to slash: " + response );
+            System.err.println("response to slash: " + response);
+            assertions(() -> {
+                assertThat(response.responseType()).isEqualTo(ResourceResponse.ResponseType.READ);
+            });
+            latch.countDown();
         });
 
         this.connector.read("/tacos", (response) -> {
-            System.err.println( "response to tacos: " + response );
+            System.err.println("response to tacos: " + response);
+            assertions(() -> {
+                assertThat(response.responseType()).isEqualTo(ResourceResponse.ResponseType.ERROR);
+            });
+            latch.countDown();
         });
 
+        latch.await();
     }
 }
