9dd839b109535abe52cc9630697134eff2f46d1b
Bob McWhirter <bob@mcwhirter.org>
2013-10-23 15:27:25 -0400

    Fix some search/replace gone wrong in module-related descriptors. Stub in a bootstrap module to launch it.

---
+++ modules/bootstrap/src/main/java/org/projectodd/restafari/bootstrap/Bootstrap.java
@@ -0,0 +1,13 @@
+package org.projectodd.restafari.bootstrap;
+
+/**
+ * @author Bob McWhirter
+ */
+public class Bootstrap {
+
+    public static void main(String...args) throws Exception {
+
+        System.err.println( "Booting up the mBaaS" );
+
+    }
+}
