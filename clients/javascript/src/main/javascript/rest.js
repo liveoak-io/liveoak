65c4ff4257602323005b2696baf996e815b819a4
Bob McWhirter <bob@mcwhirter.org>
2013-10-28 13:56:31 -0400

    Create a javascript client a little better.  Clean up cruft. Adjust dist to include javascript client in the distroy.

---
+++ clients/javascript/src/main/javascript/rest.js
@@ -0,0 +1,42 @@
+
+var mBoss = function(host, port, secure, callback) {
+  this._host = host;
+  this._port = port;
+  this._secure = secure;
+  this.stomp_client = new Stomp.Client( host, port, secure );
+
+}
+
+mBoss.prototype = {
+
+  connect: function(callback) {
+    this.stomp_client.connect( callback );
+  },
+
+  create: function(path, data) {
+    console.debug( "INSIDE CREATE" );
+    $.ajax( path, {
+      type: "POST",
+      data: JSON.stringify( data ),
+      contentType: 'application/json',
+      dataType: 'json'
+    });
+  },
+
+  read: function(path, callback) {
+  },
+
+  update: function(path, data) {
+  },
+
+  delete: function(path) {
+  },
+
+  subscribe: function(path, callback) {
+    this.stomp_client.subscribe( path, function(msg) {
+      var data = JSON.parse( msg.body );
+      callback( data );
+    } );
+  }
+
+}
