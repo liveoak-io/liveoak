653e4f61c28982fd1b5cc74b3b11aa9e4f8c2444
Ken Finnigan <ken@kenfinnigan.me>
2013-11-15 10:31:37 -0500

    Update to EPL for license

--- vertx-test-tools/src/main/java/io/liveoak/vertx/testtools/VertxTestRunner.java
+++ vertx-test-tools/src/main/java/io/liveoak/vertx/testtools/VertxTestRunner.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2013 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at http://www.eclipse.org/legal/epl-v10.html
+ */
 package io.liveoak.vertx.testtools;
 
 import org.junit.Ignore;
@@ -31,24 +36,6 @@ import java.util.Scanner;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
-
-/*
- * Copyright 2013 Red Hat, Inc.
- *
- * Red Hat licenses this file to you under the Apache License, version 2.0
- * (the "License"); you may not use this file except in compliance with the
- * License.  You may obtain a copy of the License at:
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
- * License for the specific language governing permissions and limitations
- * under the License.
- *
- *
- */
 
 /**
  * This class intercepts the running of the JUnit test and instead deploys the TestVerticle inside the Vert.x container
