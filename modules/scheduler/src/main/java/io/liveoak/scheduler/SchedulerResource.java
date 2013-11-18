3a9dcc911e60d4f3c7af867d498f323469557f10
Ken Finnigan <ken@kenfinnigan.me>
2013-11-18 16:34:45 -0500

    Fix LiveOak startup error for SchedulerResource

--- modules/scheduler/src/main/java/io/liveoak/scheduler/SchedulerResource.java
+++ modules/scheduler/src/main/java/io/liveoak/scheduler/SchedulerResource.java
@@ -24,6 +24,9 @@ import java.util.UUID;
  */
 public class SchedulerResource implements RootResource {
 
+    public SchedulerResource() {
+    }
+
     public SchedulerResource(String id) {
         this.id = id;
     }
