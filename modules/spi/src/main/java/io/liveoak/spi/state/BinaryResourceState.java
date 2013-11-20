c46dbaf5228ba86cf027732d73895d4009d1f207
Ken Finnigan <ken@kenfinnigan.me>
2013-11-20 08:43:20 -0500

    Align BinaryResourceState into correct package

--- modules/spi/src/main/java/io/liveoak/spi/state/BinaryResourceState.java
+++ modules/spi/src/main/java/io/liveoak/spi/state/BinaryResourceState.java
@@ -3,7 +3,7 @@
  *
  * Licensed under the Eclipse Public License version 1.0, available at http://www.eclipse.org/legal/epl-v10.html
  */
-package io.liveoak.spi;
+package io.liveoak.spi.state;
 
 import io.liveoak.spi.state.ResourceState;
 
