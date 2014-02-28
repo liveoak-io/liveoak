822310301c6fe11730923a9f41be97ba4ea02747
Viliam Rockai <vrockai@redhat.com>
2014-02-28 16:26:03 +0100

    CONSOLE: changes to make code jshint valid

--- console/src/main/resources/Gruntfile.js
+++ console/src/main/resources/Gruntfile.js
@@ -1,11 +1,5 @@
 'use strict';
 
-/*global module,require*/
-var lrSnippet = require('connect-livereload')();
-var mountFolder = function (connect, dir) {
-  return connect.static(require('path').resolve(dir));
-};
-
 module.exports = function (grunt) {
   // load all grunt tasks
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
--- console/src/main/resources/app/js/directives.js
+++ console/src/main/resources/app/js/directives.js
@@ -4,28 +4,28 @@
 var loDirectives = angular.module('loApp.directives', []);
 
 loDirectives.directive('loNavbar', function () {
-    return {
-        restrict: 'E',
-        replace: true,
-        templateUrl: '/admin/console/templates/lo-navbar.html',
-    }
+  return {
+    restrict: 'E',
+    replace: true,
+    templateUrl: '/admin/console/templates/lo-navbar.html',
+  };
 });
 
 loDirectives.directive('loNavigation', function () {
-    return {
-        scope: {
-            loCurrent: '@',
-            loApps: '=',
-            loApp: '='
-        },
-        restrict: 'E',
-        replace: true,
-        templateUrl: '/admin/console/templates/lo-navigation.html',
-    }
+  return {
+    scope: {
+      loCurrent: '@',
+      loApps: '=',
+      loApp: '='
+    },
+    restrict: 'E',
+    replace: true,
+    templateUrl: '/admin/console/templates/lo-navigation.html',
+  };
 });
 
 loDirectives.directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-        elm.text(version);
-    };
+  return function(scope, elm) {
+    elm.text(version);
+  };
 }]);
