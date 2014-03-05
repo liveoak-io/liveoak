5d74369f64e243c45ea2d66a3ac8b9f8049e1191
Alexandre Mendonça <ammendonca@gmail.com>
2014-03-05 10:51:39 +0000

    LIVEOAK-125: Add LiveOak theme for Keycloak login forms.

---
+++ modules/keycloak/src/main/java/io/liveoak/keycloak/theme/LiveOakLoginThemeProvider.java
@@ -0,0 +1,52 @@
+package io.liveoak.keycloak.theme;
+
+import org.keycloak.freemarker.Theme;
+import org.keycloak.freemarker.ThemeProvider;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Alexandre Mendonca
+ */
+public class LiveOakLoginThemeProvider implements ThemeProvider {
+
+    public static final String LIVEOAK = "liveoak";
+
+    private static Set<String> defaultThemes = new HashSet<String>();
+
+    static {
+        defaultThemes.add(LIVEOAK);
+    }
+
+    @Override
+    public int getProviderPriority() {
+        return 0;
+    }
+
+    @Override
+    public Theme createTheme(String name, Theme.Type type) throws IOException {
+        if (hasTheme(name, type)) {
+            return new DummyClassLoaderTheme(name, type);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public Set<String> nameSet(Theme.Type type) {
+        if (type == Theme.Type.LOGIN /* || type == Theme.Type.ACCOUNT */) {
+            return defaultThemes;
+        } else {
+            return Collections.emptySet();
+        }
+    }
+
+    @Override
+    public boolean hasTheme(String name, Theme.Type type) {
+        return nameSet(type).contains(name);
+    }
+
+}
