df6ac6e05c0c6f5b8900954f7f4fae9d6a152e7f
Alexandre Mendonça <ammendonca@gmail.com>
2014-03-26 23:54:10 +0000

    Several enhancements to LiveOak Console

--- console/src/main/resources/app/js/controllers/global.js
+++ console/src/main/resources/app/js/controllers/global.js
@@ -13,6 +13,20 @@ loMod.controller('GlobalCtrl', function($log, $rootScope, $scope, LiveOak, LoApp
   $scope.authenticated = LiveOak.auth.authenticated;
   /* jshint ignore:end */
 
+  var reIPv4String = '(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)';
+  var reIPv6String = '\\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3}))|:)))(%.+)?\\s*';
+  var rePortString = '(6553[0-5]|655[0-2]\\d|65[0-4]\\d\\d|6[0-4]\\d{3}|[1-5]\\d{4}|[1-9]\\d{0,3})';
+  var reHostString = '[\\w\\d]([\\w\\d-]*[\\w\\d])*(\\.[\\w\\d]([\\w\\d-]*[\\w\\d])*)*';
+
+  $rootScope.reIPv4 = new RegExp('^' + reIPv4String + '$');
+  $rootScope.reIPv6 = new RegExp('^' + reIPv6String + '$');
+  $rootScope.rePort = new RegExp('^' + rePortString + '$');
+  $rootScope.reHost = new RegExp('^' + reHostString + '$');
+  $rootScope.reIPv4IPv6 = new RegExp('^' + reIPv4String + '$|^' + reIPv6String + '$');
+  $rootScope.reIPv4Host = new RegExp('^' + reIPv4String + '$|^' + reHostString + '$');
+  $rootScope.reIPv6Host = new RegExp('^' + reIPv6String + '$|^' + reHostString + '$');
+  $rootScope.reIPv4IPv6Host = new RegExp('^' + reIPv4String + '$|^' + reIPv6String + '$|^' + reHostString + '$');
+
   LoAppList.get(function(data){
     $rootScope.applications = data._members;
   });
--- console/src/main/resources/app/js/directives.js
+++ console/src/main/resources/app/js/directives.js
@@ -185,3 +185,25 @@ loDirectives.directive('loSelect', function($timeout) {
     }
   };
 });
+
+loDirectives.directive('loHttpPrefix', function() {
+  return {
+    restrict: 'A',
+    require: 'ngModel',
+    link: function(scope, element, attrs, controller) {
+      function ensureHttpPrefix(value) {
+        // Need to add prefix if we don't have http:// prefix already AND we don't have part of it
+        if(value && !/^(http):\/\//i.test(value) && 'http://'.indexOf(value) === -1) {
+          controller.$setViewValue('http://' + value);
+          controller.$render();
+          return 'http://' + value;
+        }
+        else {
+          return value;
+        }
+      }
+      controller.$formatters.push(ensureHttpPrefix);
+      controller.$parsers.splice(0, 0, ensureHttpPrefix);
+    }
+  };
+});
