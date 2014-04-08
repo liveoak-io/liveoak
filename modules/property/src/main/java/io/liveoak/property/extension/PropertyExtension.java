e73996bbed82fdd467292e913b7e48735a37d17b
Gabriel Cardoso <gabrielc.cardoso@gmail.com>
2014-04-07 22:11:29 -0300

    Application settings / application client pages

---
+++ modules/property/src/main/java/io/liveoak/property/extension/PropertyExtension.java
@@ -0,0 +1,25 @@
+package io.liveoak.property.extension;
+
+import io.liveoak.spi.extension.ApplicationExtensionContext;
+import io.liveoak.spi.extension.Extension;
+import io.liveoak.spi.extension.SystemExtensionContext;
+
+/**
+ * @author Ken Finnigan
+ */
+public class PropertyExtension implements Extension {
+    @Override
+    public void extend(SystemExtensionContext context) throws Exception {
+
+    }
+
+    @Override
+    public void extend(ApplicationExtensionContext context) throws Exception {
+
+    }
+
+    @Override
+    public void unextend(ApplicationExtensionContext context) throws Exception {
+
+    }
+}
