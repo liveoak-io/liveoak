1f9e8149705a7944e4f0dacb6b4409a92beed504
Alexandre Mendonça <amendonc@redhat.com>
2014-04-08 22:27:05 +0100

    LIVEOAK-189: 'Basic' option for 'Create Application' wizard.

--- console/src/main/resources/app/js/directives.js
+++ console/src/main/resources/app/js/directives.js
@@ -216,7 +216,7 @@ loDirectives.directive('loHttpPrefix', function() {
     link: function(scope, element, attrs, controller) {
       function ensureHttpPrefix(value) {
         // Need to add prefix if we don't have http:// prefix already AND we don't have part of it
-        if(value && !/^(http):\/\//i.test(value) && 'http://'.indexOf(value) === -1) {
+        if(value && !/^(https?):\/\//i.test(value) && ('http://'.indexOf(value) === -1 && 'https://'.indexOf(value) === -1)) {
           controller.$setViewValue('http://' + value);
           controller.$render();
           return 'http://' + value;
