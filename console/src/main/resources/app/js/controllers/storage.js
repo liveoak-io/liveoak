c4422133754a1e7e556706cca3b6bbc3da7b84db
Viliam Rockai <vrockai@redhat.com>
2014-04-09 12:50:13 +0200

    LIVEOAK-128 Add all CRUD ops to the console collection page

--- console/src/main/resources/app/js/controllers/storage.js
+++ console/src/main/resources/app/js/controllers/storage.js
@@ -152,31 +152,72 @@ loMod.controller('StorageListCtrl', function($scope, $rootScope, $log, $routePar
 
   $scope.resources = [];
 
-  for(var i =0; i < loStorageList._members.length; i++){
+  if (loStorageList._members) {
+    for (var i = 0; i < loStorageList._members.length; i++) {
 
-    var resource = loStorageList._members[i];
+      var resource = loStorageList._members[i];
 
-    if (resource.hasOwnProperty('db')){
-      $scope.resources.push({
-        provider: 'Mongo DB',
-        path: resource.id,
-        host: resource.servers[0].host,
-        port: resource.servers[0].port,
-        database: resource.db
-      });
+      if (resource.hasOwnProperty('db')) {
+        $scope.resources.push({
+          provider: 'Mongo DB',
+          path: resource.id,
+          host: resource.servers[0].host,
+          port: resource.servers[0].port,
+          database: resource.db
+        });
+      }
     }
   }
 
 });
 
-loMod.controller('StorageCollectionCtrl', function($scope, $rootScope, $log, $route, currentApp,
-                                                   currentCollectionList, LoCollection, $routeParams, LoCollectionItem) {
+loMod.controller('StorageCollectionCtrl', function($scope, $rootScope, $log, $route, currentApp, $modal, Notifications,
+                                                   currentCollectionList, LoCollection, $routeParams, LoCollectionItem,
+                                                   LiveOak) {
 
   $log.debug('StorageCollectionCtrl');
 
   $rootScope.curApp = currentApp;
-  $scope.collectionList = currentCollectionList._members;
 
+  $scope.breadcrumbs = [
+    {'label': 'Applications',  'href':'#/applications'},
+    {'label': currentApp.name, 'href':'#/applications/' + currentApp.id},
+    {'label': 'Storage',       'href':'#/applications/' + currentApp.id + '/storage'}
+  ];
+
+  $scope.collectionList = currentCollectionList._members;
+  $scope.collectionData = {};
+  $scope.collectionDataBackup = {};
+  $scope.live = {};
+
+  $scope.columnsHidden = [];
+  $scope.newRow = {};
+  $scope.rowsToDelete = [];
+
+  $scope.isColHidden = false;
+
+  // TODO - seems to be redundant because with each column change, there's a data change.
+  $scope.isColumnChange = false;
+  $scope.isClearAll = false;
+  $scope.isDataChange = false;
+
+  // Filter for hidden columns
+  $scope.notHidden = function(item) {
+    if ($scope.columnsHidden.indexOf(item) === -1){
+      return true;
+    }
+    return false;
+  };
+
+  // Filter for the id column
+  $scope.notId = function(item) {
+    if (item !== 'id'){
+      return true;
+    }
+    return false;
+  };
+
+  // Search query string
   $scope.predicate = 'id';
 
   $scope.setFilter = function(predicate, reverse){
@@ -184,27 +225,46 @@ loMod.controller('StorageCollectionCtrl', function($scope, $rootScope, $log, $ro
     $scope.reverse = reverse;
   };
 
-  var loadCollectionData = function(colId) {
-    $scope.columns = [];
-    $scope.myData = LoCollectionItem.getList({appId: currentApp.id, storageId: $routeParams.storageId, collectionId: colId});
-    $scope.myData.$promise.then(function (data) {
-      if (data._members && data._members[0]) {
-        for (var c in data._members[0]) {
-          if (c !== 'self') {
-            $scope.columns.push(c);
+  var loadCollectionData = function(colId, loadColumns) {
+
+    var dataPromise = LoCollectionItem.getList({appId: currentApp.id, storageId: $routeParams.storageId, collectionId: colId}).$promise;
+
+    dataPromise.then(function (data) {
+      if (loadColumns) {
+
+        $scope.columns = ['id'];
+
+        if (data._members && data._members[0]) {
+          for (var c in data._members[0]) {
+            if (c !== 'self' && c !== 'id') {
+              $scope.columns.push(c);
+            }
           }
         }
       }
-    });
+      $scope.columnsBackup = angular.copy($scope.columns);
+      $scope.collectionData = data;
+      $scope.collectionDataBackup = angular.copy(data);
+      $scope.isDataChange = false;
+
+      LiveOak.connect( function() {
+
+        var urlSubscribe = '/' + currentApp.id + '/' + $routeParams.storageId + '/' + colId + '/*';
+
+        LiveOak.subscribe(urlSubscribe, function (data) {
+          $log.debug('read:');
+          $log.debug(data);
+          loadCollectionData($scope.collectionId, true);
+          $scope.live = data;
+        });
+      });
+    } );
   };
 
   // If nothing is selected, select the 1st item from the list
   var selectFirst = function() {
     if ($scope.collectionList) {
-
-      if (!$scope.collectionId) {
-        $scope.collectionId = $scope.collectionList[0].id;
-      }
+      $scope.collectionId = $scope.collectionList[0].id;
     }
   };
 
@@ -223,29 +283,288 @@ loMod.controller('StorageCollectionCtrl', function($scope, $rootScope, $log, $ro
   loadCollectionList();
 
   $scope.$watch('collectionId', function(){
+      $log.debug('Collection ID changed.');
       if ( $scope.collectionId ) {
-        loadCollectionData($scope.collectionId);
+        loadCollectionData($scope.collectionId, true);
+        $scope.columnsHidden = [];
+        $scope.isColHidden = false;
+        $scope.isColumnChange = false;
+        $scope.isClearAll = false;
+        $scope.isDataChange = false;
+        $scope.rowsToDelete = [];
       }
     }
   );
 
-  $scope.create = function() {
-    LoCollection.create({appId: currentApp.id, storageId: $routeParams.storageId}, {id : $scope.newCollectionId});
-    loadCollectionList();
+  $scope.$watch('collectionData._members', function(){
+      $log.debug('Collection data changed.');
+      if(!angular.equals($scope.collectionData._members, $scope.collectionDataBackup._members)){
+        $log.debug('Collection data differs from original.');
+        $scope.isDataChange = true;
+      }
+    }, true
+  );
+
+  $scope.$watch('columns', function(){
+      $log.debug('Columns number changed.');
+      if(!angular.equals($scope.collectionData._members, $scope.collectionDataBackup._members)){
+        $log.debug('Column number differs from original.');
+        $scope.isColumnChange = true;
+      }
+    }, true
+  );
+
+  var ModalInstanceCtrl = function ($scope, $modalInstance) {
+
+    $scope.close = function () {
+      $modalInstance.close();
+    };
+
+    $scope.cancel = function () {
+      $modalInstance.dismiss('cancel');
+    };
+
   };
 
-  $scope.remove = function() {
-    LoCollection.delete({appId: currentApp.id, storageId: $routeParams.storageId, collectionId: $scope.collectionId});
+  $scope.modalRowAdd = function(){
+    $modal.open({
+      templateUrl: '/admin/console/templates/modal/storage/row-add.html',
+      controller: ModalInstanceCtrl,
+      scope: $scope
+    });
+  };
+
+  $scope.modalColumnAdd = function(){
+    $modal.open({
+      templateUrl: '/admin/console/templates/modal/storage/column-add.html',
+      controller: ModalInstanceCtrl,
+      scope: $scope
+    });
+  };
+
+  $scope.modalCollectionAdd = function(){
+    $modal.open({
+      templateUrl: '/admin/console/templates/modal/storage/collection-add.html',
+      controller: ModalInstanceCtrl,
+      scope: $scope
+    });
+  };
+
+  $scope.modalCollectionDelete = function(){
+    $modal.open({
+      templateUrl: '/admin/console/templates/modal/storage/collection-delete.html',
+      controller: ModalInstanceCtrl,
+      scope: $scope
+    });
+  };
+
+  $scope.modalDataDeleteAll = function(){
+    $modal.open({
+      templateUrl: '/admin/console/templates/modal/storage/data-clear.html',
+      controller: ModalInstanceCtrl,
+      scope: $scope
+    });
+  };
+
+  $scope.collectionCreate = function(collectionName) {
+    var newCollectionPromise = LoCollection.create({appId: currentApp.id, storageId: $routeParams.storageId},
+      {id : collectionName}).$promise;
+
+    newCollectionPromise.then(function(){
+      $scope.collectionId = collectionName;
+      loadCollectionList();
+    });
+  };
+
+  $scope.collectionDelete = function() {
+    $log.debug('Deleting collection: ' + $scope.collectionId);
+    var deletePromise = LoCollection.delete({appId: currentApp.id, storageId: $routeParams.storageId,
+      collectionId: $scope.collectionId}).$promise;
     // There's a watcher on $scope.collectionId, list will be updated from there
-    $scope.collectionId = false;
-    loadCollectionList(selectFirst);
+    deletePromise.then(function(){
+      loadCollectionList(selectFirst);
+    });
   };
 
-  $scope.removeLine = function(id) {
-    LoCollectionItem.delete({appId: currentApp.id, storageId: $routeParams.storageId,
-      collectionId: $scope.collectionId, itemId: id});
+  $scope.rowAdd = function(){
+    $log.debug('Creating new row ' + $scope.newRow);
 
-    loadCollectionData($scope.collectionId);
+    if (!$scope.collectionData._members){
+      $scope.collectionData._members = [];
+    }
+
+    $scope.collectionData._members.push($scope.newRow);
+    $scope.newRow = {};
+  };
+
+  $scope.rowRemove = function(id) {
+    $log.debug('Deleting row ' + id);
+
+    var rowToRemove = -1;
+
+    for (var k in $scope.collectionData._members){
+      var item = $scope.collectionData._members[k];
+      if (item.id === id) {
+        rowToRemove = k;
+        break;
+      }
+    }
+
+    if (rowToRemove > -1){
+      $scope.collectionData._members.splice(rowToRemove, 1);
+    } else {
+      $log.error('Unable to find item to remove. The ID \"'+rowToRemove+'\" does not exist.');
+    }
+
+    $scope.rowsToDelete.push(id);
+    $scope.isDataChange = true;
+  };
+
+  $scope.columnAdd = function(columnName){
+    $log.debug('Adding column ' + columnName );
+    if ($scope.columnNew !== '') {
+      $scope.columns.push(columnName);
+    }
+
+    for (var k in $scope.collectionData._members){
+      var item = $scope.collectionData._members[k];
+      item[columnName] = '';
+    }
+
+    $scope.isDataChange = true;
+    $scope.isColumnChange = true;
+  };
+
+  $scope.columnRemove = function(columnName){
+    $log.debug('Removing column ' + columnName );
+    var index = $scope.columns.indexOf(columnName);
+
+    if (index > -1) {
+      $scope.columns.splice(index, 1);
+
+      for (var k in $scope.collectionData._members) {
+        var item = $scope.collectionData._members[k];
+        if (item.hasOwnProperty(columnName)) {
+          item[columnName] = null;
+        }
+      }
+
+      $scope.isDataChange = true;
+      $scope.isColumnChange = true;
+    }
+  };
+
+  $scope.columnClear = function(col){
+    $log.debug('Clearing column ' + col );
+
+    for (var i in $scope.collectionData._members){
+      var row = $scope.collectionData._members[i];
+      row[col] = '';
+    }
+  };
+
+  $scope.columnHide = function(col) {
+    $scope.columnsHidden.push(col);
+    $scope.isColHidden = true;
+  };
+
+  $scope.collectionClear = function(){
+    $log.debug('Clearing collection ' + $scope.collectionId );
+
+    $scope.columns = ['id'];
+    $scope.collectionData._members = [];
+    $scope.columnsHidden = [];
+    $scope.isClearAll = true;
+  };
+
+  $scope.save = function(){
+    $log.debug('Saving collection.');
+
+    // If Clear All was clicked, no need to delete specific rows, everything will be deleted.
+    if ($scope.isClearAll) {
+      for (var j in $scope.collectionDataBackup._members){
+        var itemToClear = $scope.collectionDataBackup._members[j];
+
+        LoCollectionItem.delete({appId: currentApp.id, storageId: $routeParams.storageId,
+          collectionId: $scope.collectionId, itemId: itemToClear.id});
+      }
+    } else {
+    // Else delete specific rows only
+      for (var i in $scope.rowsToDelete) {
+        var itemToDelete = $scope.rowsToDelete[i];
+        if (itemToDelete) {
+          $log.debug('Going to delete: ' + JSON.stringify(itemToDelete));
+          LoCollectionItem.delete({appId: currentApp.id, storageId: $routeParams.storageId,
+            collectionId: $scope.collectionId, itemId: itemToDelete});
+        }
+      }
+    }
+
+    var findById = function(myId, myCollection){
+      for (var l in myCollection){
+        if (myCollection[l].id === myId){
+          return angular.copy(myCollection[l]);
+        }
+      }
+
+      return undefined;
+    };
+
+    // Then update all changed data in the table
+    for (var k in $scope.collectionData._members){
+      var item = $scope.collectionData._members[k];
+
+      var itemToSave = angular.copy(item);
+      delete itemToSave.self;
+
+      // Update existing items
+      var itemFromBackup = findById(itemToSave.id, $scope.collectionDataBackup._members);
+      if (itemFromBackup && itemFromBackup.self) {
+        delete itemFromBackup.self;
+      }
+
+      $log.debug('Inspecting original: ' + JSON.stringify(itemToSave));
+      $log.debug('Inspecting backup:   ' + JSON.stringify(itemFromBackup));
+
+      if (itemToSave.id) {
+        $log.debug('Checking for update: ' + JSON.stringify(itemToSave));
+        if (itemFromBackup && !angular.equals(itemToSave, itemFromBackup)) {
+          $log.debug('Updating: ' + JSON.stringify(itemToSave));
+          LoCollectionItem.update({appId: currentApp.id, storageId: $routeParams.storageId,
+            collectionId: $scope.collectionId, itemId: itemToSave.id}, itemToSave);
+        } else {
+          $log.debug('Not updating:        ' + JSON.stringify(itemToSave));
+        }
+      }
+      // Create new items
+      else {
+        $log.debug('Creating: ' + itemToSave);
+        LoCollectionItem.create({appId: currentApp.id, storageId: $routeParams.storageId,
+          collectionId: $scope.collectionId}, itemToSave);
+      }
+    }
+
+    $scope.columnsHidden = [];
+    $scope.isColHidden = false;
+    $scope.isColumnChange = false;
+    $scope.isClearAll = false;
+    $scope.isDataChange = false;
+    $scope.rowsToDelete = [];
+
+    loadCollectionData($scope.collectionId, true);
+  };
+
+  $scope.reset = function(){
+    $log.debug('Resetting the page.');
+    $scope.columnsHidden = [];
+    $scope.isColHidden = false;
+    $scope.isColumnChange = false;
+    $scope.isClearAll = false;
+    $scope.isDataChange = false;
+    $scope.rowsToDelete = [];
+    $scope.columns = $scope.columnsBackup;
+    $scope.collectionData = angular.copy($scope.collectionDataBackup);
+    //loadCollectionData($scope.collectionId, true);
   };
 
 });
