4fc0307320f37d1814b6015a3e2177ccab95997c
Viliam Rockai <vrockai@redhat.com>
2014-06-19 12:45:32 +0200

    LIVEOAK-138 - Collection import

--- console/src/main/resources/app/js/directives.js
+++ console/src/main/resources/app/js/directives.js
@@ -372,3 +372,14 @@ loDirectives.directive('loValidation', function($timeout) {
     }
   };
 });
+
+loDirectives.directive('ngFileSelect', function() {
+  return {
+    link: function($scope,el){
+      el.bind('change', function(e){
+        $scope.file = (e.srcElement || e.target).files[0];
+        $scope.getFile();
+      });
+    }
+  };
+});
--- console/src/main/resources/app/js/services.js
+++ console/src/main/resources/app/js/services.js
@@ -64,6 +64,59 @@ loMod.factory('Notifications', function($rootScope, $timeout, $log) {
   return notifications;
 });
 
+/*
+  FileReader service, taken from:
+  http://odetocode.com/blogs/scott/archive/2013/07/03/building-a-filereader-service-for-angularjs-the-service.aspx
+ */
+loMod.factory('FileReader', function($q) {
+  var onLoad = function(reader, deferred, scope) {
+    return function () {
+      scope.$apply(function () {
+        deferred.resolve(reader.result);
+      });
+    };
+  };
+
+  var onError = function (reader, deferred, scope) {
+    return function () {
+      scope.$apply(function () {
+        deferred.reject(reader.result);
+      });
+    };
+  };
+
+  var onProgress = function(reader, scope) {
+    return function (event) {
+      scope.$broadcast('fileProgress',
+        {
+          total: event.total,
+          loaded: event.loaded
+        });
+    };
+  };
+
+  var getReader = function(deferred, scope) {
+    var reader = new FileReader();
+    reader.onload = onLoad(reader, deferred, scope);
+    reader.onerror = onError(reader, deferred, scope);
+    reader.onprogress = onProgress(reader, scope);
+    return reader;
+  };
+
+  var readAsDataURL = function (file, scope) {
+    var deferred = $q.defer();
+
+    var reader = getReader(deferred, scope);
+    reader.readAsText(file);
+
+    return deferred.promise;
+  };
+
+  return {
+    readAsDataUrl: readAsDataURL
+  };
+});
+
 /* Loaders - Loaders are used in the route configuration as resolve parameters */
 loMod.factory('Loader', function($q) {
   var loader = {};
