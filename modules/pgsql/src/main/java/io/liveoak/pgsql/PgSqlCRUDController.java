20f5d3d7d31d49fa6965d299b72a15f64614f2ab
Marko Strukelj <marko.strukelj@gmail.com>
2014-08-26 22:57:59 +0200

    LIVEOAK-397 Postgres resource  - Improved error reporting

--- modules/pgsql/src/main/java/io/liveoak/pgsql/PgSqlCRUDController.java
+++ modules/pgsql/src/main/java/io/liveoak/pgsql/PgSqlCRUDController.java
@@ -185,10 +185,13 @@ public class PgSqlCRUDController {
             List<String> blocked = configuration.blockedSchemas();
             createNewSchema = (blocked == null || !blocked.contains(tableRef.schema())) &&
                     (exposed == null || exposed.isEmpty() || exposed.contains(tableRef.schema()));
-
-            if (!createNewSchema || !configuration.allowCreateSchema()) {
+            if (!createNewSchema) {
                 throw new ResourceProcessingException(ResourceErrorResponse.ErrorType.NOT_ACCEPTABLE,
-                        "Not allowed to create a new schema");
+                        "Not allowed access to " + tableRef.schema() + " - blocked by 'schemas' / 'blocked-schemas' configuration");
+            }
+            if (!configuration.allowCreateSchema()) {
+                throw new ResourceProcessingException(ResourceErrorResponse.ErrorType.NOT_ACCEPTABLE,
+                        "Not allowed to create a new schema - 'allow-create-schema' configuration option is false");
             }
         }
 
