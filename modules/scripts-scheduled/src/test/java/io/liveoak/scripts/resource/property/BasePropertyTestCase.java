ea803c735bd97b40898f9fc0465c0d08f882dd73
Matt Wringe <mwringe@redhat.com>
2014-10-02 16:53:38 -0400

    LIVEOAK-556: Remove files which should not have been committed into the scheduled scripts module.

--- modules/scripts-scheduled/src/test/java/io/liveoak/scripts/resource/property/BasePropertyTestCase.java
+++
@@ -1,9 +0,0 @@
-package io.liveoak.scripts.resource.property;
-
-import io.liveoak.scripts.resource.BaseResourceTriggeredTestCase;
-
-/**
- * @author <a href="mailto:mwringe@redhat.com">Matt Wringe</a>
- */
-public class BasePropertyTestCase extends BaseResourceTriggeredTestCase {
-}
