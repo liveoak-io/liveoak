f1e50be40eab65bda35303ea1a69e539dba3aab6
Viliam Rockai <vrockai@redhat.com>
2014-11-12 16:26:23 +0100

    LIVEOAK-361: multi-column search

--- console/src/main/resources/app/js/filters.js
+++ console/src/main/resources/app/js/filters.js
@@ -51,71 +51,46 @@ loFilters.filter('words', function () {
   };
 });
 
-loFilters.filter('conditional', function () {
-  return function (input, query, columns, conditions) {
-
-    var defaultFilter = function(q, data) {
-
-      var result = [];
-      angular.forEach(data, function (line) {
-        angular.forEach(line, function (cell) {
-          if (cell.indexOf && cell.indexOf(q) > -1) {
-            if (result.indexOf(line) === -1) {
-              result.push(line);
-            }
-          }
-        });
-      });
-      return result;
-    };
-
-    // If columns to filter or filtering conditions are missing, don't filter anything and return the original list
-    if (!columns || columns.length === 0) {
-      if (query) {
-        return defaultFilter(query, input);
-      } else {
-        return input;
-      }
-    }
-
+loFilters.filter('dataContains', function () {
+  return function (input, query) {
     var output = [];
-    var includes = [];
-    var excludes = [];
 
-    // Generate includes & excludes sets from conditions
-    angular.forEach(conditions, function(condition){
-      if (condition.type === 'E' && condition.value !== '') {
-        includes.push(condition.text);
-      } else if (condition.value !== ''){
-        excludes.push(condition.text);
-      }
-    });
-
-    // Actual filtering
-    angular.forEach(input, function(item) {
-      var pass = false;
-      angular.forEach(columns, function(column) {
-
-        if (item[column]) {
-          if ( query && query !== '' && item[column].indexOf(query) > -1) {
-            pass = true;
-            return;
-          }
-          if ( includes && ( includes.length > 0 && includes.indexOf(item[column]) > -1) ) {
-            pass = true;
-            return;
-          }
-          if ( excludes && ( excludes.length > 0 && excludes.indexOf(item[column]) < 0) ) {
-            pass = true;
-            return;
+    angular.forEach(input, function (line) {
+      angular.forEach(line, function (cell) {
+        if (cell.indexOf && cell.indexOf(query) > -1) {
+          if (output.indexOf(line) === -1) {
+            output.push(line);
           }
         }
       });
+    });
 
-      if (pass && output.indexOf(item) < 0){
+    return output;
+  };
+});
+
+loFilters.filter('condition', function () {
+  return function (input, conditions) {
+    var output = [];
+
+    angular.forEach(input, function(item) {
+      var pass = true;
+
+      angular.forEach(conditions, function(condition){
+        var cell = item[condition.column];
+
+        if (condition.type === 'E' && cell !== condition.text){
+          pass = false;
+        }
+
+        if (condition.type !== 'E' && cell === condition.text){
+          pass = false;
+        }
+      });
+
+      if (pass){
         output.push(item);
       }
-
     });
 
     return output;
