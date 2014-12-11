6cbdbff79d6e8210eae2bec9b6b27ed9655a5789
Matt Wringe <mwringe@redhat.com>
2014-12-11 12:08:01 -0500

    LIVEOAK-565: Perform checks on the target-path when updating a scripting resource. LIVEOAK-715: Save the script metadata file on an update.

--- modules/scripts/src/main/java/io/liveoak/scripts/resourcetriggered/resource/ResourceScriptService.java
+++ modules/scripts/src/main/java/io/liveoak/scripts/resourcetriggered/resource/ResourceScriptService.java
@@ -14,9 +14,13 @@ public class ResourceScriptService implements Service<ResourceScripts> {
 
     private ResourceScripts resourceScripts;
 
+    public ResourceScriptService(String applicationId) {
+        this.applicationId = applicationId;
+    }
+
     @Override
     public void start(StartContext startContext) throws StartException {
-        this.resourceScripts = new ResourceScripts(scriptRegistryInjector.getValue(), vertxInjector.getValue());
+        this.resourceScripts = new ResourceScripts(applicationId, scriptRegistryInjector.getValue(), vertxInjector.getValue());
     }
 
     @Override
@@ -39,4 +43,6 @@ public class ResourceScriptService implements Service<ResourceScripts> {
 
     private InjectedValue<ScriptRegistry> scriptRegistryInjector = new InjectedValue<>();
     private InjectedValue<Vertx> vertxInjector = new InjectedValue<>();
+
+    private String applicationId;
 }
