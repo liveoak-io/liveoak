35cf56148bd5bf4dd8d8561a0eb7a52b9c55dc20
Alexandre Mendonça <amendonc@redhat.com>
2014-12-17 22:46:55 +0000

    LIVEOAK-692: Patch codemirror.js file with workaround changes on build

--- console/src/main/resources/Gruntfile.js
+++ console/src/main/resources/Gruntfile.js
@@ -12,6 +12,7 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-htmlhint');
+  grunt.loadNpmTasks('grunt-patch');
 
   // configurable paths
   var projectConfig = {
@@ -29,6 +30,30 @@ module.exports = function (grunt) {
           targetDir: 'app/lib',
           verbose: true,
           layout: 'byComponent'
+        }
+      }
+    },
+    patch: {
+      default_options: {
+        options: {
+          patch:
+          'Index: codemirror.js\n' +
+          '===================================================================\n' +
+          '--- codemirror.js\n' +
+          '+++ codemirror.js\n' +
+          '@@ -3346,7 +3346,9 @@\n' +
+          '       (ie ? "rgba(255, 255, 255, .05)" : "transparent") +\n' +
+          '       "; outline: none; border-width: 0; outline: none; overflow: hidden; opacity: .05; filter: alpha(opacity=5);";\n' +
+          '     if (webkit) var oldScrollY = window.scrollY; // Work around Chrome issue (#2712)\n' +
+          '+    if (webkit) display.input.style.top = "-99999px"; /* Patched for LiveOak */\n' +
+          '     focusInput(cm);\n' +
+          '+    if (webkit) display.input.style.top = (e.clientY - 5) + "px"; /* Patched for LiveOak */\n' +
+          '     if (webkit) window.scrollTo(null, oldScrollY);\n' +
+          '     resetInput(cm);\n' +
+          '     // Adds "Select all" to context menu in FF\n'
+        },
+        files: {
+          'app/lib/codemirror/codemirror_liveoak.js': 'app/lib/codemirror/codemirror.js'
         }
       }
     },
@@ -119,6 +144,6 @@ module.exports = function (grunt) {
   ]);
 
   grunt.registerTask('default', ['build', 'watch']);
-  grunt.registerTask('mvnBuild', ['bower']);
+  grunt.registerTask('mvnBuild', ['bower', 'patch']);
   grunt.registerTask('lint', ['jshint', 'htmlhint']);
 };
