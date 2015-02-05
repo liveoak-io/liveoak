66449044318884ef5a90861b54a913ef77e3b826
Ken Finnigan <ken@kenfinnigan.me>
2015-02-05 14:11:23 -0500

    LIVEOAK-607 Install git resource into admin application.json - Add git resource to application.json of admin app - Add scripts to create git repo for admin app as part of distr process

---
+++ dist/src/main/resources/scripts/admin-git-repo.bat
@@ -0,0 +1,4 @@
+cd target/liveoak/apps/admin
+git init
+git add -A
+git commit -m "Initial install of admin application"
---
+++ dist/src/main/resources/scripts/admin-git-repo.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cd target/liveoak/apps/admin
+git init
+git add -A
+git commit -m "Initial install of admin application"
